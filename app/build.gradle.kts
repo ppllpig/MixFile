488f0fbefc092f0d946efd2b3e8f57051175973c
1 <1>
2024-10-11 23:51:03 +0800

    修复A1线路无法下载

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.donut.mixfile"
         minSdk = 24
         targetSdk = 34
-        versionCode = 50
-        versionName = "1.5.2"
+        versionCode = 51
+        versionName = "1.5.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
