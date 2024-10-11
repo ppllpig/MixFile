de66a8e104e071a7c7eb6dde0467f942c0eb76e9
1 <1>
2024-10-11 15:39:56 +0800

    优化重命名

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.donut.mixfile"
         minSdk = 24
         targetSdk = 34
-        versionCode = 49
-        versionName = "1.5.1"
+        versionCode = 50
+        versionName = "1.5.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
